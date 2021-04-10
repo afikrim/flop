17792251f8de78adb9e699815638ecf0363efe9e
AFM - Home <afikrim@deliv.co.id>
2021-04-10 13:57:28 +0700

    oops! something modified in authController and i missed it

--- src/main/java/com/github/afikrim/flop/auth/AuthController.java
+++ src/main/java/com/github/afikrim/flop/auth/AuthController.java
@@ -1,24 +1,14 @@
 package com.github.afikrim.flop.auth;
 
-import java.util.List;
-
-import com.github.afikrim.flop.users.User;
 import com.github.afikrim.flop.users.UserRequest;
 import com.github.afikrim.flop.utils.response.Response;
 import com.github.afikrim.flop.utils.response.ResponseCode;
-import com.github.afikrim.flop.wallets.Wallet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
