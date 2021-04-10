afe4d731991ae0699df8ab2dd7e22e007ce322a9
AFM - Home <afikrim@deliv.co.id>
2021-04-10 13:37:13 +0700

    add system wallet entity and its repository

---
+++ src/main/java/com/github/afikrim/flop/systemwallets/SystemWallet.java
@@ -0,0 +1,115 @@
+package com.github.afikrim.flop.systemwallets;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.github.afikrim.flop.wallets.Wallet;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.hateoas.RepresentationModel;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "system_wallets")
+@AllArgsConstructor
+@NoArgsConstructor
+public class SystemWallet extends RepresentationModel<SystemWallet> implements Serializable {
+
+    private static final long serialVersionUID = 555055814305292278L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "wallet_id")
+    private Wallet wallet;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "balance")
+    private Long balance;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(Wallet wallet) {
+        this.wallet = wallet;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
---
+++ src/main/java/com/github/afikrim/flop/systemwallets/SystemWalletRepository.java
@@ -0,0 +1,13 @@
+package com.github.afikrim.flop.systemwallets;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface SystemWalletRepository extends JpaRepository<SystemWallet, Long> {
+
+    @Query("SELECT sw FROM SystemWallet sw WHERE sw.phone = ?1 AND sw.wallet.id = ?2")
+    public Optional<SystemWallet> findByPhoneAndWalletId(String phone, Integer walletId);
+
+}
